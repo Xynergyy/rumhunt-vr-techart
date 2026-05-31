f520b6bdb3bcf45f5a8c003b79dc7f7c8315fa37
pariyakorn saelao <pariyakorn.carrot28@gmail.com>
2026-05-31 22:53:02 +0700

    Animation Controller & Blend tree
    waiting for ...
    Animation clip

---
+++ Assets/Asset/Character/GhostAnimController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GhostAnimController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetVisible(false);
+    }
+
+    public void SetVisible(bool visible)
+    {
+        gameObject.SetActive(visible);
+    }
+
+    public void PlayOpening()
+    {
+        SetVisible(true);
+        animator.SetTrigger("TriggerOpening");
+        PlayOpeningVFX(); // เรียก VFX
+    }
+
+    private void PlayOpeningVFX()
+    {
+        // TODO: เปิด particle วงล้อเวท
+        // TODO: FloatUp layer จะทำงานอัตโนมัติอยู่แล้ว
+    }
+
+    public void PlayRandomDance()
+    {
+        int index = Random.Range(0, 5);
+        animator.SetInteger("DanceIndex", index);
+    }
+
+    public void PlayRandomAttack()
+    {
+        int index = Random.Range(0,2);
+        animator.SetInteger("AttackIndex", index);
+    }
+
+    public void PlayHurt_01() => animator.SetTrigger("TriggerHurt_01");
+    public void PlayHurt_02() => animator.SetTrigger("TriggerHurt_02");
+
+    public void PlayEnding(bool playerWin)
+    {
+        if (playerWin)
+            animator.SetTrigger("TriggerEndWin");
+        else
+            animator.SetTrigger("TriggerEndLose");
+    }
+}
